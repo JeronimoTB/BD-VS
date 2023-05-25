ee8e326a7ef30d10bd7fcdde7dacbf230bb50dc6
JeronimoTB <jerotobe14@gmail.com>
2023-05-25 11:43:42 -0500

    Toma

---
+++ Automoviles/Modelo/Config.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Config
+    {
+        //Metodo Anticuado y NO RECOMENDADO
+        //static string cadenaConexion = @"Data Source=pale-HP;Initial Catalog=Vehiculos; User ID=wil; Password=jero2maria";
+        static string cadenaConexion = @"Data Source=MEDREAFSTCDS012; Initial Catalog=Transportes;Trusted_Connection=true";
+
+        public static string ConexionConexion
+        {
+            get {return cadenaConexion; }
+        }   
+    }
+}
---
+++ Automoviles/Modelo/TipoVehiCRUD.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Modelo
+{
+    public class TipoVehiCRUD
+    {
+        //Insert
+        public static SqlCommand CrearComandoProc()
+        {
+            string _cadenaConexion = Configuracion.CadenaConexion;
+            SqlConnection _conexion = new SqlConnection(_cadenaConexion);
+            SqlCommand _comando = new SqlCommand("regisTipoVehic", _conexion);
+            _comando.CommandType = CommandType.StoredProcedure;
+            return _comando;
+        }
+
+        //Read (List)
+        public static SqlCommand CrearComando()
+        {
+            string _cadenaConexion = Configuracion.CadenaConexion;
+            SqlConnection _conexion = new SqlConnection();
+            _conexion.ConnetionString = _cadenaConexion;
+            SqlCommand _comando = new SqlCommand();
+            _comando = _conexion.CreateCommand();
+            _comando.CommandType = CommandType.Text;
+            return _comando;
+        }
+
+        public static DataTable EjecutarComandoSelect(SqlCommand comando)
+        {
+            DataTable _tabla = new DataTable();
+            try
+            {
+                comando.Connection.Open();
+                SqlDataAdapter adaptador = new SqlDataAdapter();
+                adaptador.SelectCommand = comando;
+                adaptador.Fill(_tabla);
+            }
+            catch (Exception ex)
+            { throw ex; }
+            finally
+            { comando.Connection.Close(); }
+            return _tabla;
+
+            throw new NotImplementedException();
+        }
+    }
+}
