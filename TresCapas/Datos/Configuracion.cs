f35b07a4092287266f219045d36c5830f0ac19a8
JeronimoTB <105673028+JeronimoTB@users.noreply.github.com>
2023-05-29 11:46:48 -0500

    BD DE WILSON
    Trae todo lo cambiado y es el Visual Studio

---
+++ TresCapas/Datos/Configuracion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class Configuracion
+    {
+        //static string cadenaConexion = @"Data Source=pale-HP;Initial Catalog=Vehiculos;User ID=wil;Password=jero2maria";
+        static string cadenaConexion = @"Data Source=MEDREAFSTCDS012;Initial Catalog=Automovil;Trusted_Connection=true";
+        public static string CadenaConexion
+        {
+            get { return cadenaConexion; }
+        }
+
+    }
+}
